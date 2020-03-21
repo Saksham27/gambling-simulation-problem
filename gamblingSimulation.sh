67483b9642a3ac35eb30efbb0febaacc7d0c92c3
Saksham27 <geekysaksham27@gmail.com>
2020-03-21 09:09:11 +0530

    Added: total amount won or lost after playin 20 days

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -6,9 +6,14 @@ BET=1
 GAMBLE_WON=1 # so GAMBLE_LOST will be 0
 STOP_AT_MIN_STAKE=50
 STOP_AT_MAX_STAKE=150
+DAYS_TO_PLAY_IN_MONTH=20
 
 # variables
 stake=0
+day=1
+totalWon=0
+totalLost=0
+
 # function for gamble game
 # param1 : curret availible stakes
 function GambleGame() {
@@ -21,9 +26,21 @@ function GambleGame() {
 	fi
 }
 
-currentStake=$START_STAKE_FOR_DAY
 
-while [ $currentStake -gt $STOP_AT_MIN_STAKE ] && [ $currentStake -lt $STOP_AT_MAX_STAKE ]
+while [ $day -le $DAYS_TO_PLAY_IN_MONTH ]
 do
-	GambleGame $currentStake
+	currentStake=$START_STAKE_FOR_DAY
+	while [ $currentStake -gt $STOP_AT_MIN_STAKE ] && [ $currentStake -lt $STOP_AT_MAX_STAKE ] # stopping for day
+	do
+		GambleGame $currentStake
+	done
+
+	if [ $currentStake -eq $STOP_AT_MIN_STAKE ]
+	then
+		totalLost=$(( $totalLost+$START_STAKE_FOR_DAY-$currentStake ))
+	else
+		totalWon=$(( $totalWon+$currentStake--$START_STAKE_FOR_DAY ))
+	fi
+	(( day++ ))
 done
+
